3a5f6067da6d840db9058bf8719c97be80d65741
Fabiano Rong <fabianorong@gmail.com>
2024-10-29 16:59:37 -0300

    add navbar

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/navbar';
 import './global.css';
 
 export const metadata = {
@@ -12,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
